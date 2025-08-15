5923f4b5557b1b5222aa97533be0aa604b1c27ea
ahmed <aieisalem17@gmail.com>
2025-08-15 12:22:03 +0100

    Update wezterm

--- home/wezterm.lua
+++ home/wezterm.lua
@@ -1,14 +1,22 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font_size = 26
+config.font_size = 24
 config.font = wezterm.font 'Iosevka'
 
+config.keys = {
+   {
+      key = 'Backspace', mods = 'CTRL',
+      action = act.SendKey {key = 'Backspace', mods = 'ALT'}
+   }
+}
 config.hide_tab_bar_if_only_one_tab = true
+config.enable_kitty_keyboard = true
 
 return config
